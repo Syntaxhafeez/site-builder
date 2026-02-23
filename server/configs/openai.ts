1bed170df18c0ffc510fdbc7cea8f234f46fb603
Syntaxhafeez <syntaxhafeez@gmail.com>
2026-02-23 14:30:46 +0530

    backend pre deployment fix

--- server/configs/openai.ts
+++ server/configs/openai.ts
@@ -1,10 +1,12 @@
+export const runtime = "nodejs";
+
 import OpenAI from "openai";
 
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.AI_API_KEY,
   defaultHeaders: {
-    "HTTP-Referer": "http://localhost:5173",
+    "HTTP-Referer": "https://site-builder-fawn.vercel.app",
     "X-Title": "AI Website Builder"
   }
 });
